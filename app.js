360b19d25db72fe16485ab2979decf3e3f4e1b4b
valentindj <valentindj@starway-media.com>
2023-10-13 13:11:31 +0300

    controllers and views added for models

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
 
 const connectMongoose = require('./database/mongoose');
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 //DB CONNECTION
 connectMongoose().catch((error) => console.log(error));
